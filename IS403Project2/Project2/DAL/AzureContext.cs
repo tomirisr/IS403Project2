0109d45377919682745f0ab8a752089cd1bed624
Tomiris Mollinet <tomiris.rad@gmail.com>
2017-12-13 22:56:27 -0700

    added project2 with original files

---
+++ IS403Project2/Project2/DAL/AzureContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Project2.Models;
+using System.Data.Entity;
+
+namespace Project2.DAL
+{
+    public class AzureContext : DbContext
+    {
+        public AzureContext() : base("AzureContext")
+        {
+
+        }
+
+        public DbSet<Mission> Missions { get; set; }
+        public DbSet<MissionQuestion> MissionQuestions { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserMissionQuestion> UserMissionQuestions { get; set; }
+    }
+
+ 
+}
---
+++ IS403Project2/Project2/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Project2.Models
+{
+    [Table("WebUser")]
+    public class User
+    {
+       [Key]
+       public int UserID { get; set; }
+       public string UserFirstName { get; set; }
+        public string UserLastName { get; set; }
+        public string UserEmail { get; set; }
+        public string UserPassword { get; set; }
+    }
+}
